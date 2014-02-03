0d33ba0d6b9d5387b810dd806b7dfcd12ea4d124
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-02-03 22:06:31 +0200

    implemnt upload result

--- src/GoWeb/Wa/Response/UploadResult.php
+++ src/GoWeb/Wa/Response/UploadResult.php
@@ -6,5 +6,36 @@ use \GoWeb\Wa\Structure;
 
 class UploadResult extends Structure
 {
+    private $_key;
     
+    /**
+     * Set file's input name
+     * 
+     * @param type $key
+     */
+    public function setKey($key)
+    {
+        $this->_key = $key;
+        return $this;
+    }
+    
+    public function getStatus()
+    {
+        return $this->get('extra.uploads.' . $this->_key . '.status');
+    }
+    
+    public function isErrorOccured()
+    {
+        return 0 !== $this->getStatus();
+    }
+    
+    public function getErrorMessage()
+    {
+        return $this->get('extra.uploads.' . $this->_key . '.strerror');
+    }
+    
+    public function getId()
+    {
+        return $this->get('extra.uploads.' . $this->_key . '.id');
+    }
 }
